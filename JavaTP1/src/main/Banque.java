dd9a1c6f658b90d39f743dcab9c4574e2d53c75d
Alexis <Alexis@172.17.62.95>
2018-02-14 10:47:09 +0100

    osef

--- JavaTP1/src/main/Banque.java
+++ JavaTP1/src/main/Banque.java
@@ -2,6 +2,18 @@ package main;
 
 public class Banque {
 	private int nbClient=0;
-	private Client [] Clients= new Client[nbClient];
+	private Client [] clients= new Client[100];
 	
+	public void ajouterClient() {
+		if(this.nbClient-1<100) {
+			nbClient++;
+			clients[nbClient].ajouterCompte();
+		}
+	}
+	
+	public void afficherBilan() {
+		for(int i=0;i<nbClient;i++) {
+			this.clients[i].afficherBilan();
+		}
+	}
 }
