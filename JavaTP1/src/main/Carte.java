745b4a5a49cc918939e95897232a57812158c424
Alexis <Alexis@172.17.62.95>
2018-02-14 11:03:05 +0100

    osef

---
+++ JavaTP1/src/main/Carte.java
@@ -0,0 +1,21 @@
+package main;
+
+public class Carte {
+	private int code[]=new int[4];
+	private Banque banqueAsso;
+	private int numClient;
+	private int numCompte;
+	private Date dateValidite;
+	private int compteur=3;
+	
+	boolean codeValide(int[] codeVer) {
+		if(codeVer==code) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	void payer(Banque b, int numClient,int numCompte, float montant) {
+		
+	}
+}
